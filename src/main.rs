23bbc8c55ff935695360e13b0510cae75897e730
= <=>
2026-06-06 09:52:16 +0100

    added test

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use clap::{Parser, Subcommand};
+use std::fs::File;
+use std::io::prelude::*;
 
 #[derive(Parser)]
 #[command(name = "myapp", author, version, about, long_about = None)]
@@ -19,9 +21,34 @@ enum Commands {
         /// Number of columns to split image into
         #[arg(short, long, default_value_t = 0)]
         columns: u32
+    },
+    WriteToFile {
+        #[arg(long)]
+        filepath: String,
     }
+
 }
 
 fn main() {
     println!("Hello, world!");
+
+    let args = Args::parse();
+
+    match args.command {
+        Some(Commands::Grid { filepath, rows, columns }) => {
+
+        },
+        Some(Commands::WriteToFile { filepath }) => {
+            let _ = write_to_file(filepath, "hello".to_string());
+        },
+        None => {
+            std::process::exit(1);
+        }
+    }
+}
+
+fn write_to_file(filepath: String, content: String)  -> std::io::Result<()> {
+    let mut file = File::create(filepath)?;
+    file.write_all(content.as_bytes())?;
+    Ok(())
 }
---
+++ tests/grid_test.rs
@@ -0,0 +1,31 @@
+use assert_cmd::cargo::*; // Import cargo_bin_cmd! macro and methods
+//use predicates::prelude::*; // Used for writing assertions
+#[cfg(test)]
+mod grid_test {
+    use assert_cmd::cargo_bin_cmd;
+
+    #[test]
+    fn should_create_2x2() -> Result<(), Box<dyn std::error::Error>> {
+        let mut cmd = cargo_bin_cmd!();
+
+        cmd.arg("grid")
+            .arg("--rows 3").arg("--columns 3")
+            .arg("--filepath dog.png");
+        // cmd.assert()
+        //     .failure()
+        //     .stderr(predicate::str::contains("could not read file"));
+
+        Ok(())
+    }
+
+    #[test]
+    fn should_write_to_file() -> Result<(), Box<dyn std::error::Error>> {
+        let mut cmd = cargo_bin_cmd!();
+
+        cmd.arg("writetofile")
+            .arg("--filepath").arg("hello.txt");
+
+        Ok(())
+    }
+
+}
