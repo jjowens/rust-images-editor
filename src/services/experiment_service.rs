c949fca34c43bb365a8e92463e064d542ab0c6e0
= <=>
2026-08-11 22:51:59 +0100

    update hue rotate and added blind effects

--- src/services/experiment_service.rs
+++ src/services/experiment_service.rs
@@ -1,4 +1,5 @@
-use image::{DynamicImage, GenericImage, GenericImageView, Luma, SubImage};
+use std::path::Path;
+use image::{DynamicImage, GenericImage, GenericImageView};
 
 pub fn experiment_service(open_file_path: String, save_file_path: String) -> Result<(), String> {
     println!("Experiment with: {}", open_file_path);
@@ -54,4 +55,58 @@ fn update_image(mut img: DynamicImage, rotate_val: i32) -> DynamicImage {
     img = img.unsharpen(12.0, 0);
 
     img
+}
+
+pub fn experiment_huerotate_service(open_file_path: String, save_directory_path: String, file_name: String)  -> Result<(), String> {
+    println!("Experiment with: {}", open_file_path);
+
+    let mut img = image::open(open_file_path.clone()).unwrap();
+
+    for degree in 1..360 {
+        img = img.huerotate(degree);
+
+        let new_save_file_path = [save_directory_path.as_str(), file_name.as_str(), "_", degree.to_string().as_str(), ".png"].concat();
+        let _ = img.save(new_save_file_path).unwrap();
+    }
+
+    Ok(())
+}
+
+pub fn experiment_huerotate_separate_service(open_file_path: String, save_directory_path: String, file_name: String)  -> Result<(), String> {
+    println!("Experiment with: {}", open_file_path);
+
+    for degree in 1..360 {
+        println!("Degrees: {}", degree);
+        let mut img = image::open(open_file_path.clone()).unwrap();
+        //img = img.huerotate(degree);
+        img = update_image(img.clone(), degree);
+
+        let new_save_file_path = [save_directory_path.as_str(), file_name.as_str(), "_", degree.to_string().as_str(), ".png"].concat();
+        let _ = img.save(new_save_file_path).unwrap();
+    }
+
+    Ok(())
+}
+
+pub fn experiment_blinds_effect_service(open_file_path: String, save_file_path: String)  -> Result<(), String> {
+    println!("Blinds experiment with: {}", open_file_path);
+
+    let mut img = image::open(open_file_path).unwrap();
+
+    let mut canvas = image::RgbaImage::new(img.width(), img.height());
+
+    let total_height = img.height() - 100;
+
+    //let subimage = img.sub_image(0,0,img.width(),img.height()).to_image();
+
+    canvas.copy_from(&img.grayscale().to_rgba8(), 0, 0);
+
+    let subimage = img.sub_image(0,25,img.width(),total_height).to_image();
+    canvas.copy_from(&subimage, 0, 25);
+
+    let _ = canvas.save(save_file_path).unwrap();
+
+    subimage.save("./test-output/dumdum.png").unwrap();
+
+    Ok(())
 }
--- tests/unit_experiment_test.rs
+++ tests/unit_experiment_test.rs
@@ -1,6 +1,6 @@
 #[cfg(test)]
 mod unit_experiment_test {
-    use rustimageseditor::services::experiment_service::{experiment_subimage_service, experiment_warhol_service};
+    use rustimageseditor::services::experiment_service::{experiment_blinds_effect_service, experiment_huerotate_separate_service, experiment_huerotate_service, experiment_subimage_service, experiment_warhol_service};
 
     #[test]
     fn experiment_subimage_test() {
@@ -22,5 +22,35 @@ mod unit_experiment_test {
         assert!(output.is_ok());
     }
 
+    #[test]
+    fn experiment_huerotate_test() {
+        let open_file_path = "test-images/experiment/dog1.png".to_string();
+        let save_directory_path = "test-output/experiment/huerotate/".to_string();
+        let save_file_name = "dog".to_string();
 
+        let output = experiment_huerotate_service(open_file_path, save_directory_path, save_file_name);
+
+        assert!(output.is_ok());
+    }
+
+    #[test]
+    fn experiment_huerotate_separate_test() {
+        let open_file_path = "test-images/experiment/dog1.png".to_string();
+        let save_directory_path = "test-output/experiment/hue-rotate-separate/".to_string();
+        let save_file_name = "dog".to_string();
+
+        let output = experiment_huerotate_separate_service(open_file_path, save_directory_path, save_file_name);
+
+        assert!(output.is_ok());
+    }
+
+    #[test]
+    fn experiment_blinds_effect_test() {
+        let open_file_path = "test-images/experiment/dog1.png".to_string();
+        let save_file_path = "test-output/experiment/dog_blinds_effect.png".to_string();
+
+        let output = experiment_blinds_effect_service(open_file_path, save_file_path);
+
+        assert!(output.is_ok());
+    }
 }
